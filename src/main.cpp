e97f1a7fad27fb821f2983e8e0c526b5f388d698
nick2090afk <eee19387271@uniwa.gr>
2026-02-04 15:36:17 +0200

    add some comments

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include "ArduinoJson.h"
 #include "secrets.h" 
 
-// UUIDs for Coospo
+// UUIDs for Coospo HW807
 #define HR_SERVICE_UUID "0000180d-0000-1000-8000-00805f9b34fb"
 #define HR_CHAR_UUID "00002a37-0000-1000-8000-00805f9b34fb"
 #define BAT_SERVICE_UUID "0000180f-0000-1000-8000-00805f9b34fb"
